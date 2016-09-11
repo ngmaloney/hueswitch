5e51c1533831a893064d7c47710aa37cd440adc4
clach04 <Chris.Clark@actian.com>
2016-09-10 22:15:44 -0700

    Clay support

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,9 @@
 
 var UI = require('ui');
 var Settings = require('settings');
+var Clay = require('./clay');  // NOTE From Clay readme, Pebble.js does not currently support message keys so you will have to use v0.1.7 of Clay
+var clayConfig = require('./config');
+var clay = new Clay(clayConfig, null, {autoHandleEvents: false});
 var Ajax = require('ajax');
 var BulbCtrl = require('bulb_ctrl');
 
@@ -18,39 +21,63 @@ Settings.option({
   user: "newdeveloper"
 });
 
+
+function GetSwitchList()
+{
+    Ajax(
+      {
+        url: "http://" + Settings.option("hub_ip") + "/api/" + Settings.option("user") + "/lights",
+        type: 'json'
+      },
+      function(data) {
+        var items = [];
+        
+        for(var i in data) {
+          items.push({title: data[i].name, id: i});
+        }
+        
+        var menu = new UI.Menu({
+          sections: [{
+            title: "Bulb Selection",
+            items: items
+          }]
+        });
+        
+        menu.on('select', function(e) {
+          BulbCtrl.render(e.item.id); 
+        });
+        
+        menu.show();
+      },
+      function(error) {
+        console.log("ERROR: " + error);
+      }
+    );
+}
+
+Pebble.addEventListener('showConfiguration', function(e) {
+  Pebble.openURL(clay.generateUrl());
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  if (e && !e.response) {
+    return;
+  }
+  var dict = clay.getSettings(e.response);
+    console.log('e.response: ' + e.response);
+    console.log('e.response.length: ' + e.response.length);
+    console.log('dictionary to validate ' + JSON.stringify(dict));
+
+  // Save the Clay settings to the Settings module. 
+  Settings.option(dict);
+
+  // Connect and get switch/light information
+  GetSwitchList();
+});
+
 var card = new UI.Card({
   title: "Hue Switch",
   body: "Loading bulb list."
 });
 
 card.show();
-
-Ajax(
-  {
-    url: "http://" + Settings.option("hub_ip") + "/api/" + Settings.option("user") + "/lights",
-    type: 'json'
-  },
-  function(data) {
-    var items = [];
-    
-    for(var i in data) {
-      items.push({title: data[i].name, id: i});
-    }
-    
-    var menu = new UI.Menu({
-      sections: [{
-        title: "Bulb Selection",
-        items: items
-      }]
-    });
-    
-    menu.on('select', function(e) {
-      BulbCtrl.render(e.item.id); 
-    });
-    
-    menu.show();
-  },
-  function(error) {
-    console.log("ERROR: " + error);
-  }
-);
